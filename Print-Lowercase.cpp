84b2591900798aaa0f4df24a3b78e4436669a545
Bruce Lin <brucelin1218@gmail.com>
2017-06-15 21:28:09 -0700

    Create Print-Lowercase.cpp

---
+++ Print-Lowercase.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char s[15] = "HELLo WOrLd!";
+    
+    for (int i=0; s[i]; i++) {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+            cout << char(s[i] - 'A' + 'a');
+        else
+            cout << s[i];
+    }
+    cout << "\n";
+    return 0;
+}
+
+// output: hello world!
+
