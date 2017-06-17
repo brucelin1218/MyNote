3dfae5b6aa518a4b0b98fe551f547935d713a392
Bruce Lin <brucelin1218@gmail.com>
2017-06-16 22:26:12 -0700

    Create ArraySize.cpp

---
+++ ArraySize.cpp
@@ -0,0 +1,15 @@
+// A fast way to return the actual array size by only incrementing one single variable.
+
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int arr[100], a, n = 0;
+    
+    while(scanf("%d", &a), a) {
+        arr[n++] = a;
+    }
+    
+    printf("The size of array is: %d\n", n);
+    return 0;
+}
