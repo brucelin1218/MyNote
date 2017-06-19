ae531b8184b11a0131c0b951b1653159828e7ecb
Bruce Lin <brucelin1218@gmail.com>
2017-06-18 18:53:42 -0700

    Create histogram.cpp

---
+++ histogram.cpp
@@ -0,0 +1,25 @@
+// We can create the lookup talbe by hash table or binary search tree when 
+// there apears a huge number.
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+    int array[10] = { 1, 3, 4, 10, 11,
+                      1000000000, 23, 99, 123, 514 };
+                      
+    // Instead of creating a huge array, we use map
+    //  int c[1000000000] = {};
+    map<int, int> c;    // binary search tree
+    
+    // count the frequency
+    for (int i=0; i<10; i++)
+        c[array[i]]++;
+    
+    // print the result
+    for (auto i: c)
+        cout << i.first << ':' << i.second << '\n';
+    
+    return 0;
+}
