039f6ad3576888edb9bcfbdd3a1e830e0313c5bb
Bruce Lin <brucelin1218@gmail.com>
2017-07-10 18:08:33 -0700

    Update Vector-vs-Array-Sort.cpp

--- Vector-vs-Array-Sort.cpp
+++ Vector-vs-Array-Sort.cpp
@@ -13,13 +13,13 @@ int main() {
     printf("\nSort by descending order:\n");
     sort(v.rbegin(), v.rend());
     for(i = v.begin(); i != v.end(); i++) {
-        printf("%d ", *i);
+        printf("%d ", *i);  // 23 9 5 4 1 
     }
     
     printf("\n\nSort by ascending order:\n");
     sort(v.begin(), v.end());
     for(i = v.begin(); i != v.end(); i++) {
-        printf("%d ", *i);
+        printf("%d ", *i);  // 1 4 5 9 23
     }
     
     printf("\n\n===== Sort with array: =====\n");
@@ -27,25 +27,25 @@ int main() {
     sort(arr, arr+5);
     reverse(arr, arr+5);
     for (int i=0; i<5; i++) {
-        printf("%d ", arr[i]);
+        printf("%d ", arr[i]);  // 23 9 5 4 1 
     }
     
     printf("\n\nSort by ascending order:\n");
     reverse(arr, arr+5);
     for (int i=0; i<5; i++) {
-        printf("%d ", arr[i]);
+        printf("%d ", arr[i]); // 1 4 5 9 23
     }
     
     printf("\n\n===== Shuffle the content with vector: =====\n");
     random_shuffle(v.begin(), v.end());
     for (i = v.begin(); i != v.end(); i++) {
-        printf("%d ", *i);
+        printf("%d ", *i);  // 23 9 1 5 4
     }
     
     printf("\n\n===== Partial sort with vector: =====\n");
     partial_sort(v.begin(), v.begin()+3, v.end());
     for (i = v.begin(); i != v.end(); i++) {
-        printf("%d ", *i);
+        printf("%d ", *i);  // 1 4 5 23 9 
     }
     
     return 0;
