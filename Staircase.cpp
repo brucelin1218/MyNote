e087a3e71ea757076c7735397c05417df30b8d15
Bruce Lin <brucelin1218@gmail.com>
2017-09-28 22:20:44 -0700

    Create Staircase.cpp

---
+++ Staircase.cpp
@@ -0,0 +1,54 @@
+/*
+* Consider a staircase of size : 4
+*
+*       #
+*      ##
+*     ###
+*    ####
+*
+* Size: 6 
+*
+*      #
+*     ##
+*    ###
+*   ####
+*  #####
+* ######
+*
+*/
+
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  int N;
+  scanf("%d", &N);
+  char arr[N][N];
+  // Fill up the 2D array with space.
+  for (int i=0; i<N; i++)
+    for (int j=0; j<N; j++)
+      arr[i][j] = ' ';
+  // Use for counting index
+  int cnt = 1;
+  // When j == N-cnt, that is the start to print '#'
+  for (int i=0; i<N; i++) {
+    for (int j=0; j<N; j++) {
+      if (j == N-cnt) {
+        int m = j;
+        while (m<N) {
+          cout << '#';
+          m++;
+        }
+      }
+      cout << arr[i][j];
+    }
+    cout << endl;
+    cnt++;
+  }
+  
+  return 0;
+}
