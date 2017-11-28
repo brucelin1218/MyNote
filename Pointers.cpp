23d6eb3dd3ab36c6c4e18c9f76f2a441642d39c5
Bruce Lin <brucelin1218@gmail.com>
2017-11-28 01:12:54 -0800

    Update Pointers.cpp

--- Pointers.cpp
+++ Pointers.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 
 int main () {
-  int firstvalue = 5, secondvalue = 15;
-  int *p1, *p2;
+  int a = 1, b = 2;
+  int* ptr1;
+  int* ptr2;
 
-  p1 = &firstvalue;  // p1 = address of firstvalue
-  p2 = &secondvalue; // p2 = address of secondvalue
-  *p1 = 10;          // value pointed to by p1 = 10
-  *p2 = *p1;         // value pointed to by p2 = value pointed to by p1
-  p1 = p2;           // p1 = p2 (value of pointer is copied)
-  *p1 = 20;          // value pointed to by p1 = 20
+  ptr1 = &a;      // ptr1 = address of firstvalue
+  ptr2 = &b;      // ptr2 = address of secondvalue
+  *ptr1 = 10;     // value pointed to by ptr1, 10
+  *ptr2 = *ptr1;  // value pointed to by ptr2 = value pointed to by ptr1
+  ptr1 = ptr2;    // ptr1 = ptr2 (value of pointer is copied)
+  *ptr1 = 20;     // value pointed to by p1, 20
   
-  cout << "firstvalue is " << firstvalue << '\n';
-  cout << "secondvalue is " << secondvalue << '\n';
+  cout << "the value of a is " << a << '\n'; // 10
+  cout << "the value of b is " << b << '\n'; // 20
   
   return 0;
 }
