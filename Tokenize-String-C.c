0093690064d5fc1dc910d4e69ed443a0075e3fbd
Bruce Lin <brucelin1218@gmail.com>
2018-01-28 15:54:06 -0800

    Update Tokenize-String-C.c

--- Tokenize-String-C.c
+++ Tokenize-String-C.c
@@ -1,13 +1,27 @@
+/**
+  This simple program tokenizes a string and stores the tokens into a char* ptr1 array,
+  Then copy the string from ptr1[i] into ptr2[i].
+**/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(int argc, const char * argv[]) {
   char str[] = "This is  a test only - just a test";
+  char *ptr1[100] = {};
+  char *ptr2[100] = {};
+  int index1 = 0, index2 = 0;
   char *token = strtok(str, " "); // Get the first token.
   
   while (token != NULL) { 
-    printf("%s\n", token);
+    ptr[index1] = token;
+    ptr2[index2] = malloc(sizeof(token)); // Memory allocation for the strcpy().
+    printf("%ld\n", &ptr1[index1]); // Print the address of pointers.
+    printf("%s\n", ptr2[index2]); // Print the content of the pointers.
+    free(ptr2[index2]); // Free ptr2[index2] memory 
+    index1++;
+    index2++;
     token = strtok(NULL, " "); // Passing NULL so it keeps walking through the next token.
   }
   
@@ -15,12 +29,21 @@ int main(int argc, const char * argv[]) {
 }
 
 // Output
-This
-is
-a
-test
-only
--
-just
-a
-test
+// 140732920754784
+// This
+// 140732920754792
+// is
+// 140732920754800
+// a
+// 140732920754808
+// test
+// 140732920754816
+// only
+// 140732920754824
+// -
+// 140732920754832
+// just
+// 140732920754840
+// a
+// 140732920754848
+// test
